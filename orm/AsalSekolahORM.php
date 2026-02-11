3e81c6e60dc43ef69041453f7bcc8ce4c5d6e9cc
andpromobile <andpromobile@gmail.com>
2026-02-11 09:38:58 +0800

    Add AsalSekolahORM class with table definition; include config requirement

---
+++ orm/AsalSekolahORM.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'config.php';
+
+class AsalSekolahORM extends Model
+{
+//nama tabel di database
+public static $_table = 'asal_sekolah';
+}
--- orm/ProfileORM.php
+++ orm/ProfileORM.php
@@ -1,6 +1,8 @@
 <?php
+require_once 'config.php';
+
 class ProfileORM extends Model
 {
 //nama tabel di database
-public static $_table = 'profile';
+    public static $_table = 'profile';
 }
